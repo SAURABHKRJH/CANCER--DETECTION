d3be8eec2639659e442c9c0354536186873d40aa
SAURABHKRJH <54108084+SAURABHKRJH@users.noreply.github.com>
2019-09-15 23:46:39 +0530

    Add files via upload

---
+++ cancer.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 11 12:23:39 2019
+
+@author: saurabh
+"""
+
+
+from keras.models import Sequential # initialize neural network
+from keras.layers import Convolution2D # making CNN to deal with image for video we use 3d
+from keras.layers import MaxPooling2D # for proceed poooling step
+from keras.layers import Flatten #convert pool to feature 
+from keras.layers import Dense #create and connect nn
+from keras.callbacks import ModelCheckpoint
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('H:/project/test',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('H:/project/test',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         samples_per_epoch = 8000,
+                         nb_epoch = 4,
+                         validation_data = test_set,
+                         nb_val_samples = 2000)
+
+#prediction
+
+classifier.save('H:/project/cancer/infected_model_bak.h5')
+from tensorflow.keras.models import load_model
+classifier = load_model('H:/project/cancer/infected_model_bak.h5')
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+import numpy as np
+img1 = image.load_img('H:/project/test/uninfected/uninfected (3).png', target_size=(64, 64))
+img = image.img_to_array(img1)
+img = img/255
+# create a batch of size 1 [N,H,W,C]
+img = np.expand_dims(img, axis=0)
+prediction = classifier.predict(img, batch_size=None,steps=1) #gives all class prob.
+print(prediction)
+if(prediction[:,:]>0.5):
+    value ='uninfected :%1.2f'%(prediction[0,0])
+    plt.text(20, 62,value,color='red',fontsize=18,bbox=dict(facecolor='white',alpha=0.8))
+else:
+    value ='infected :%1.2f'%(1.0-prediction[0,0])
+    plt.text(20, 62,value,color='red',fontsize=18,bbox=dict(facecolor='white',alpha=0.8))
+
+plt.imshow(img1)
+plt.show()
